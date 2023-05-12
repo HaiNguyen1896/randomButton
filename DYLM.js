8d104db3a1819c06f18dca0f40bb53b89bf95e6d
HaiNguyen1896 <132861075+HaiNguyen1896@users.noreply.github.com>
2023-05-13 01:32:25 +0700

    Update lan 1

---
+++ DYLM.js
@@ -0,0 +1,11 @@
+function yes(){
+    alert("<3");
+}
+function no(){
+    let x= document.getElementById("no1");
+    let xpos = Math.random() * window.innerWidth;
+    let ypos= Math.random() * window.innerHeight;
+    x.style.left = xpos + 'px';
+    x.style.top= ypos +'px';
+    document.getElementById("")
+}
